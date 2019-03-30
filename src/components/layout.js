e9e0113c6590c13a12a3255ecb2420081ffcc2c5
Nikhil Hira <nikhil@leancode.io>
2019-03-30 14:43:29 -0700

    add sass plugin

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@
  * Layout component that queries for data
  * with Gatsby's StaticQuery component
  *
- * See: https://www.gatsbyjs.org/docs/static-query/
+ * See: https:   //www.gatsbyjs.org/docs/static-query/
  */
 
 import React from "react"
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import "./layout.scss"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -28,9 +28,9 @@ const Layout = ({ children }) => (
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
           style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
+            margin    : `0 auto`,
+            maxWidth  : 960,
+            padding   : `0px 1.0875rem 1.45rem`,
             paddingTop: 0,
           }}
         >
