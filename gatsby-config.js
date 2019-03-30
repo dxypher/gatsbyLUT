9f326fe6d50c09635dcd25e8bc4e5965eb1ee83a
Nikhil Hira <nikhil@leancode.io>
2019-03-30 15:13:50 -0700

    start adding markdown blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,28 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sass`,
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name            : `gatsby-starter-default`,
